59d906eb6cdf412a26a63b5c39ed42ffa502da1e
Stanislav Smirnov <stanislav.smirnov@griffithuni.edu.au>
2022-10-09 16:40:13 +1000

    add .env file to store environment variales and corresponding initializer in main.go file

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+// Initialize and load environment variales
+func init() {
+
+    err := godotenv.Load(".env")
+
+    if err != nil {
+		fmt.Print(err)
+    }
+}
 
 func main() {
 	router := gin.Default()
